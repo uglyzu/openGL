5303b2a4af26cab5f879d9df3bb8ec6b3c997624
栾盼 <luanpan@aobi.com>
2018-05-25 16:06:55 +0800

    上传两个工程文件

---
+++ oglpg8/Project1/Project1/triangles.cpp
@@ -0,0 +1,112 @@
+
+
+///////////////////////////////////////////////////////////////////////
+//
+// triangles.cpp
+//
+///////////////////////////////////////////////////////////////////////
+#if _MSC_VER>=1900
+#include "stdio.h"
+_ACRTIMP_ALT FILE* __cdecl __acrt_iob_func(unsigned);
+#ifdef __cplusplus
+extern "C"
+#endif
+FILE* __cdecl __iob_func(unsigned i) {
+	return __acrt_iob_func(i);
+}
+#endif /* _MSC_VER>=1900 */
+///////////////////////////////////////////////////////////
+
+
+#include <iostream>
+using namespace std;
+
+#include "vgl.h"
+#include "LoadShaders.h"
+
+enum VAO_IDs { Triangles, NumVAOs };
+enum Buffer_IDs { ArrayBuffer, NumBuffers };
+enum Attrib_IDs { vPosition = 0 };
+
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
+
+const GLuint NumVertices = 6;
+//---------------------------------------------------------------------
+//
+// init
+//
+
+void init(void)
+{
+	glGenVertexArrays(NumVAOs, VAOs);
+	glBindVertexArray(VAOs[Triangles]);
+
+	GLfloat vertices[NumVertices][2] = {
+		{ -0.90, -0.90 }, // Triangle 1
+		{ 0.85, -0.90 },
+		{ -0.90, 0.85 },
+		{ 0.90, -0.85 }, // Triangle 2
+		{ 0.90, 0.90 },
+		{ -0.85, 0.90 }
+	};
+
+	glGenBuffers(NumBuffers, Buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),
+		vertices, GL_STATIC_DRAW);
+
+	ShaderInfo shaders[] = {
+		{ GL_VERTEX_SHADER, "triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);
+
+	glVertexAttribPointer(vPosition, 2, GL_FLOAT,
+		GL_FALSE, 0, BUFFER_OFFSET(0));
+	glEnableVertexAttribArray(vPosition);
+
+	glClearColor(1, 1, 1, 1);
+}
+
+//---------------------------------------------------------------------
+//
+// display
+//
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBindVertexArray(VAOs[Triangles]);
+	glDrawArrays(GL_POINTS, 0, NumVertices);
+	glFlush();
+}
+
+//---------------------------------------------------------------------
+//
+// main
+//
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(512, 512);
+	glutInitContextVersion(4, 3);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutCreateWindow(argv[0]);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit()) {
+		cerr << "Unable to initialize GLEW ... exiting" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	init();
+
+	glutDisplayFunc(display);
+
+	glutMainLoop();
+}
